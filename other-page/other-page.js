08fabb46ff3cba3574a109446dc5051580f98d8a
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-16 12:41:19 -0800

    completed functionality with sign up, sign in and log out

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,16 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
+import { checkAuth, fetchPosts, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logOutButton = document.getElementById('logout');
+
+logOutButton.addEventListener('click', async () => {
+    await logout();
+});
+
+window.addEventListener('load', async () => {
+    const posts = await fetchPosts();
+    console.log(posts, 'posts');
+});
