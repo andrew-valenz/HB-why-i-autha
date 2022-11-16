6090206e89f1c95d898569cf66124b882631fb7e
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-16 11:37:22 -0800

    added functions in fetch utils

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ const signInForm = document.getElementById('sign-in');
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = newformData(signUpForm);
+    const user = await signupUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        location.replace('other-page');
+    }
+});
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://runrnxgdrbkaytuydcss.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnJueGdkcmJrYXl0dXlkY3NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMDAsImV4cCI6MTk4MzY4NDAwMH0.eTaenRCbb6KMg82PtWyN6Md7lu-lSFCxPx-a9-y8WJw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -9,11 +10,25 @@ export function getUser() {
 }
 
 export async function signupUser(email, password) {}
-
+const response = await client.auth.signUp({ email, password });
+return response.user;
+if (response.use) {
+    return response.user;
+} else {
+    console.error(response.error);
+}
 export async function signInUser(email, password) {}
 
-export async function checkAuth() {}
+export async function checkAuth() {
+    const user = getUser();
+    if (!user) location.replace('/');
+}
 
-export async function redirectIfLoggedIn() {}
+export async function redirectIfLoggedIn() {
+    const user = getUser();
+}
 
-export async function logout() {}
+export async function logout() {
+    await client.auth.signOut();
+    return (window.location.href = '/');
+}
