8ab5152f14df3008af6aa416c848837cb940eea1
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-16 11:45:15 -0800

    completed DOM elements in app js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+import { redirectIfLoggedIn, signInUser, signUpUser } from './fetch-utils.js';
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
+redirectIfLoggedIn();
+
 // Wire up sign in and sign up forms to supabase
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const data = newformData(signUpForm);
-    const user = await signupUser(data.get('email'), data.get('password'));
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const user = await signUpUser(email, data.get('password'));
 
     if (user) {
         location.replace('other-page');
@@ -14,3 +18,14 @@ signUpForm.addEventListener('submit', async (e) => {
 });
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const email = data.get('email');
+    const user = await signInUser(email, data.get('password'));
+
+    if (user) {
+        location.replace('/other-page');
+    }
+});
